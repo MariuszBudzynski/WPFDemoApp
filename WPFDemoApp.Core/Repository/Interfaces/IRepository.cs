7b56399810d0438088afc6612b82d005db41bfa1
Hood <mariusz.budzynski87@gmail.com>
2024-05-27 09:43:57 +0200

    Added SaveSingleDataItemUseCase

--- WPFDemoApp.Core/Repository/Interfaces/IRepository.cs
+++ WPFDemoApp.Core/Repository/Interfaces/IRepository.cs
@@ -3,6 +3,6 @@
 	public interface IRepository
 	{
 		Task<IEnumerable<TEntity>> GetAllDataAsync<TEntity>() where TEntity : class, IEntityHasBeenDeleted;
-
+		Task SaveSingleDataItem<TEntity>(TEntity data) where TEntity : class, IEntityHasBeenDeleted;
 	}
 }
---
+++ WPFDemoApp.Core/UseCases/SaveSingleDataItemUseCase.cs
@@ -0,0 +1,25 @@
+﻿namespace WPFDemoApp.Core.UseCases
+{
+	internal class SaveSingleDataItemUseCase
+	{
+		private readonly IRepository _repository;
+		private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
+		public SaveSingleDataItemUseCase(IRepository repository)
+        {
+			_repository = repository;
+		}
+
+		public async Task ExecuteAsync<TEntity>(TEntity data) where TEntity : class, IEntityHasBeenDeleted
+		{
+			try
+			{
+				await _repository.SaveSingleDataItem<TEntity>(data);
+			}
+			catch (Exception ex)
+			{
+				_logger.Error(ex, "An error occurred while processing data.");
+				throw;
+			}
+		}
+	}
+}
